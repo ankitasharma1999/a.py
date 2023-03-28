4384ea2c3fa0662571615f819365f73e63afd8d4
ankitasharma1999 <98891495+ankitasharma1999@users.noreply.github.com>
2023-03-28 19:02:03 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,60 @@
+#List
+l=[10,34,"ankita",12.56]
+print(l)
+print(l[2])
+print(l[-2])
+index=l.index(10)
+print(index)
+
+#Tuple
+t=(22,45,78,"appple")
+print(t)
+
+#Dictionary
+d={"fruit":"mango","price":150}
+print(d)
+print(d["price"])
+
+#Sets
+s={23,34,56,34}
+print(s)
+f={"Apple","banana","mango"}
+print(f)
+
+#Typecasting
+
+#int to str
+b=10
+c=str(b)
+print(c)
+print(type(c))
+
+#int to float
+g=2
+c=float(g)
+print(c)
+print(type(c))
+
+#str to int
+g="123"
+c=int(g)
+print(c)
+print(type(c))
+
+#str to float
+g="123"
+c=float(g)
+print(c)
+print(type(c))
+
+#float to int
+g=23.6
+c=int(g)
+print(c)
+print(type(c))
+
+#float to str
+g=23.66
+c=str(g)
+print(c)
+print(type(c))
